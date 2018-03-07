6e7d60eff670da74adc6738193fdfd828995c42b
xp <xp@CETYSDAM020>
2018-03-07 18:22:19 +0100

    falta warmup 1 y 2

---
+++ src/codingbatindividual/CodingBatIndividual.java
@@ -0,0 +1,14 @@
+package codingbatindividual;
+/**
+ * @author Gonzalo Izuzquiza
+ */
+public class CodingBatIndividual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
